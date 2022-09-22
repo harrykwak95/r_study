5c2b6ad7168fb8eb60da35ac7b500de219afc41d
GWAKMW\user <gwakmw@duse.co.kr>
2022-09-22 20:13:33 +0900

    리액트 공부 첫번째 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+    let [docTitle, setDocTitle] = useState(['코트 추천', '맛집 추천', '여행지 추천']);
+    let [logo, setLogo] = useState('ReactBlog');
+    let [like, setLike] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="black-nav">
+            <h4 style={{color:'white', fontSize : '30px'}}>{logo}</h4>
+        </div>
+        <button onClick={()=>{
+            let copy = [...docTitle];
+            copy = [ '나나 나나나', '가가 가가가', '다다 다다다'];
+            copy = copy.sort();
+            setDocTitle(copy)}}>글 수정</button>
+        <div className="list">
+            <h4>{docTitle[0]} <span onClick={()=>setLike(like+1)}>👍</span>{like}</h4>
+            <p>2022-09-20 발행</p>
+        </div>
+        <div className="list">
+            <h4>{docTitle[1]} <span onClick={()=>setLike(like+1)}>👍</span>{like}</h4>
+            <p>2022-09-21 발행</p>
+        </div>
+        <div className="list">
+            <h4>{docTitle[2]} <span onClick={()=>setLike(like+1)}>👍</span>{like}</h4>
+            <p>2022-09-21 발행</p>
+        </div>
+
     </div>
   );
 }
